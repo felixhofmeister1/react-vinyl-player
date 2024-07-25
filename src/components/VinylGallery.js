ab450a8a16a08cfd400052a53b87cb5a17005f55
Felix Hofmeister <felixhofmeister24673@gmail.com>
2024-07-25 14:47:23 +0200

    vinyl-gallery

---
+++ src/components/VinylGallery.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const VinylGallery = () => {
+  return (
+    <div>
+      <h1>Vinyl Collection</h1>
+      <img src="/images/metallica-vinyl.png" alt="Metallica Vinyl" />
+      <img src="/images/acdc-vinyl.png" alt="AC/DC Vinyl" />
+      <img src="/images/queen-vinyl.png" alt="Queen Vinyl" />
+      <img src="/images/abbey-road-vinyl.png" alt="Abbey Road Vinyl" />
+    </div>
+  );
+};
+
+export default VinylGallery;
